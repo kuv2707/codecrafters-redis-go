f178885ec777e1bc9ead11b52008c50ad671267a
Kislay <kislayudbhav.verma.cd.eee22@itbhu.ac.in>
2024-05-23 14:09:59 +0530

    added support for multiple PING

--- app/server.go
+++ app/server.go
@@ -19,15 +19,17 @@ func main() {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
-	conn, err := l.Accept()
-	defer l.Close()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
-	} else {
-		fmt.Println("Connection accepted successfully!")
+	for {
+		conn, err := l.Accept()
+		defer l.Close()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			os.Exit(1)
+		} else {
+			fmt.Println("Connection accepted successfully!")
+		}
+		go handleConnection(conn)
 	}
-	handleConnection(conn)
 }
 
 func handleConnection(conn net.Conn) {
